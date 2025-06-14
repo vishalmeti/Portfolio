d124a7f9ac045e056af577d10129dfe9ab0f03cd
vishalmeti <vishalmeti9@gmail.com>
2025-06-14 11:49:17 +0530

    fix: update ResumeLink to the correct URL

--- portfolio/Data/Links.jsx
+++ portfolio/Data/Links.jsx
@@ -1,4 +1,4 @@
-export const ResumeLink = 'https://drive.google.com/file/d/1YVzaM2CCIpCanp3slyPrXtec-48ws5V_/view?usp=sharing'
+export const ResumeLink = 'https://drive.google.com/file/d/1vgVdrY-K4Hwkh9YjXZ9YtlN88MrsB35n/view?usp=sharing'
 
 export const LinkedInLink = 'https://www.linkedin.com/in/vishal-meti-280876219/'
 
